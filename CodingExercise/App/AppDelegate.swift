890546fd3c352b59b95a428ff5ad7cf1b51eb3f3
Slack Candidate <candidate@slack.com>
2022-02-27 11:14:18 -0800

    migrate to scenedelegate; introduce simple dependency container to construct view controllers; fix string wrapping on message view

---
+++ CodingExercise/App/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CodingExercise
+//
+//  Copyright © 2021 slack. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
